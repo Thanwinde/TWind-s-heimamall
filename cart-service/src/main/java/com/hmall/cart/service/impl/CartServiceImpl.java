a63d60f8b5ee7991f0ba4ad1dfe88d7515af0fce
TWind <thanwinde@outlook.com>
2025-03-10 14:15:51 +0800

    采用了nacos作为注册中心，实现了集群通信

--- cart-service/src/main/java/com/hmall/cart/service/impl/CartServiceImpl.java
+++ cart-service/src/main/java/com/hmall/cart/service/impl/CartServiceImpl.java
@@ -1,6 +1,7 @@
 package com.hmall.cart.service.impl;
 
 import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.RandomUtil;
 import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -15,6 +16,8 @@ import com.hmall.common.utils.BeanUtils;
 import com.hmall.common.utils.CollUtils;
 import com.hmall.common.utils.UserContext;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -40,9 +43,9 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements ICartService {
 
-    //TODO private final IItemService itemService;
-
     private  final RestTemplate restTemplate;
+
+    private  final DiscoveryClient discoveryClient;
 
     @Override
     public void addItem2Cart(CartFormDTO cartFormDTO) {
@@ -90,8 +93,13 @@ public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements IC
         // 1.获取商品id
         Set<Long> itemIds = vos.stream().map(CartVO::getItemId).collect(Collectors.toSet());
         // 2.查询商品
+        List<ServiceInstance> instances = discoveryClient.getInstances("item-service");
+        if(CollUtils.isEmpty(instances)){
+            return;
+        }
+        ServiceInstance serviceInstance = instances.get(RandomUtil.randomInt(instances.size()));
         ResponseEntity<List<ItemDTO>> ids = restTemplate.exchange(
-                "http://localhost:8080/items?ids={ids}",
+                serviceInstance.getUri() + "/items?ids={ids}",
                 HttpMethod.GET,
                 null,
                 new ParameterizedTypeReference<List<ItemDTO>>() {
